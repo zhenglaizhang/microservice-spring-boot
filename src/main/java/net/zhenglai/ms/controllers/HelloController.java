7e3256c2c50ed60062304b511221dee3855e2c2e
Zhenglai Zhang <zhenglai.zhang@aiack.com>
2018-08-28 18:10:34 +0800

    refactor hello api

---
+++ src/main/java/net/zhenglai/ms/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package net.zhenglai.ms.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "hello")
+public class HelloController {
+
+    @RequestMapping(value = "/{firstName}/{lastName}", method = RequestMethod.GET)
+    public String hello(@PathVariable("firstName") String firstName,
+                        @PathVariable("lastName") String lastName) {
+        return String.format("{\"message\": \"Hello %s %s\"}", firstName, lastName);
+    }
+}
