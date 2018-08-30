6d675ec4c075d9d4c8b406b6f7fef83d98d3af95
Zhenglai Zhang <zhenglai.zhang@aiack.com>
2018-08-30 18:33:22 +0800

    use git and fix spring cloud deps

--- licensing-service/src/main/java/net/zhenglai/licenses/Application.java
+++ licensing-service/src/main/java/net/zhenglai/licenses/Application.java
@@ -2,8 +2,10 @@ package net.zhenglai.licenses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
